122dca3487ac057e445faf62e2fe3454a964ba44
Reshmithaa  B <97106645+reshmithaa@users.noreply.github.com>
2023-10-15 11:52:31 +0530

    Update Data.kt

--- pass/Data.kt
+++ pass/Data.kt
@@ -2,7 +2,11 @@ package com.example.sqlite
 
 import java.io.Serializable
 
-class Data(n: String,p: String) : Serializable{
+class Data(t: String, n: String,p: String, w:String, note: String, c: String) : Serializable{
     var name = n
     var password = p
+    var title = t
+    var website = w
+    var note = note
+    var category = c
 }
